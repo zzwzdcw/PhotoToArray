6db17e0bf15f86dace0b7b7789464bcee0455399
zzwzdcw <zzwzdcw@users.noreply.github.com>
2020-02-25 15:16:23 +0800

    核心功能以及完成

--- PhotoToArray.cpp
+++ PhotoToArray.cpp
@@ -1,11 +1,15 @@
-﻿
+﻿// 图片最大像素宽度应限制在128以内；否则修改       temp16bit的数组上限
+// 本程序用于ssd1306 128*64 oled显示器
+// 
 #include <opencv2/core.hpp>
+#include <opencv2/opencv.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2\imgproc.hpp>
 #include<string>
 #include <iostream>
 #include <fstream>
 #include<cmath>;
+
 
 using namespace std;
 using namespace cv;
@@ -36,8 +40,7 @@ Mat Grayscale(Mat img) {
     return out;
 }
 //二值化函数
-//
-Mat Binarize(Mat img, int th) {   
+Mat Binarize(Mat img, int th) {
     //th为阈值0-255
     int width = img.cols;//宽
     int height = img.rows;//高
@@ -70,7 +73,7 @@ string to16(int data)
     char tempFirst = '0';
     char tempSecond = '0';
     String ReturnDate = "0X";
-    
+    // cout << "10   to  16" << endl;
     if (data < 16)
     {
         tempSecond = h[data];
@@ -84,8 +87,8 @@ string to16(int data)
         tempSecond = h[tempData];
         return ReturnDate + tempFirst + tempSecond;
     }
-    
-   
+
+
 }
 //二进制转十进制
 int to10(int data)
@@ -93,7 +96,7 @@ int to10(int data)
     //最大二进制数为 11111111
     int power = 0;
     int tempdata = 0;
-    int enddata=0;
+    int enddata = 0;
     for (int i = 0; i < 8; i++)
     {
         tempdata = data % 10;
@@ -108,73 +111,102 @@ int to10(int data)
             power = 0;
         }
 
+
     }
+    // cout << "2   to  10" << endl;
     return enddata;
+}
+
+//用于bit像素数据反转s
+void BitInversion(int temp16bit[16], int maxtemp16bit) {
+    // String BitInversionString=" ";
+  //  cout << "BitInversion" << endl;
+
+    for (; maxtemp16bit >= 0; maxtemp16bit--)
+    {
+
+        os << to16(to10(temp16bit[maxtemp16bit])) << ",";
+        //      cout << to16(to10(temp16bit[maxtemp16bit])) << endl;
+
+    }
+    // return BitInversionString;
+}
+
+int GetMaxBit(Mat img) {
+    int Maxbit = 0;
+    string BitInversionString;
+    if ((img.rows % 8) == 0)
+    {
+        Maxbit = img.rows / 8;
+    }
+    else
+    {
+        Maxbit = (img.rows / 8) + 1;
+    }
+    //  cout << "获得了最大值" << Maxbit << endl;
+    return Maxbit;
+
 }
 //图片转化数组
 void toArray(Mat img) {
     String Array = "const unsigned char gImage []= {";      //输出数组的开头
     String end = "};";                                       //输出数组的结尾
     String comma = ",";                                      //分割各个十六进制数
-    String temp8bit;                                        //暂存8位的二进制数（字符型）
     int tempint;                                            //暂存8位的二进制数（整形）
     char temp;                                             //暂存一位二进制数
-    int i = 0;                      
+    int temp16bit[16];                                    //用于bit像素数据反转暂存
+    int i = 0;
+    String temp8Bit;                                        //暂存8位的二进制数（字符型）
+
+    int Maxbit = GetMaxBit(img);        //行数/8
+    int bit = 0;
     os << Array;
-    for (int y = 0; y < img.rows; y++)
-      //从上到下
-    //for (int x = 0; x < img.cols; x++)  //从上到下
+    for (int y = 0; y < img.rows; y++)//从上到下
     {
+
         for (int x = 0; x < img.cols; x++)
-        //for (int y = 0; y < img.rows; y++)      //z y
         {
             //图片黑色部分是黑色
-            
-            if (img.at<uchar>(y, x)>Value)      //(img.at<uchar>(rows, cols)
-            {
-                temp= '0';      //0为灯灭
-            }
-            else
-            {
 
-                temp= '1';       //1位灯亮
-            }
-            
-            /**
-            //图片白色部分是白色
             if (img.at<uchar>(y, x) > Value)      //(img.at<uchar>(rows, cols)
             {
-
-                temp = '1';        //1位灯亮
+                temp = '0';      //0为灯灭
             }
             else
             {
-                temp = '0';         //0为灯灭
 
+                temp = '1';       //1位灯亮
             }
-            */
-            temp8bit = temp8bit + temp;
-            
-            i++; 
-            if (y==img.rows)        //行结束清零
+
+            temp8Bit = temp8Bit + temp;             //把0 1 的数暂存到temp16bit中；
+            i++;
+
+            if (x == img.cols - 1)        //行结束清零
             {
-                tempint = stoi(temp8bit);
-                os << to16(to10(tempint));//先转化成十进制然后转化成十六进制
+                tempint = stoi(temp8Bit);
+                temp16bit[bit] = tempint;           //行结束时开始进行翻转
+             //   cout << "行结束了" << temp8Bit << endl;
+                BitInversion(temp16bit, bit);
+                bit = 0;
                 i = 0;
-                temp8bit = "";
-                os << comma;
+                temp8Bit = "";
+
+
             }
             if (i == 8)     //8位二进制数得到后清零
             {
-                tempint = stoi(temp8bit);
-                os << to16(to10(tempint));//先转化成十进制然后转化成十六进制
+                tempint = stoi(temp8Bit);
+                temp16bit[bit] = tempint;
+                bit++;
+                //     cout << "得到了8位像素了：" << temp8Bit << endl;
                 i = 0;
-                temp8bit ="";
-                os << comma;
+                temp8Bit = "";
             }
-            
+
+            // cout << "toArray" << endl;
+
         }
-    }   
+    }
     os << end;
     os.close();
 }
@@ -184,10 +216,10 @@ void toArray(Mat img) {
 int main()
 {
     os.open("array.txt");//将对象与文件关联
-   // Mat img = imread("text64_64.jpg");//读取图片
-    Mat img = imread("5.jpg");//读取图片
+    Mat img = imread("text64_64.jpg");//读取图片
     Mat GrayscaleImg = Grayscale(img);//得到灰度图
     Mat BinarizeImg = Binarize(GrayscaleImg, Value);//二值化
+    Mat flip_horizontal_img;
     namedWindow("显示原始图片", 0);//可变大小窗口
     imshow("显示原始图片", img);
     namedWindow("显示灰度图片", 0);
@@ -195,9 +227,10 @@ int main()
     namedWindow("显示二值化图片", 0);
     imshow("显示二值化图片", BinarizeImg);
     cout << "正在生成中请耐心等待" << endl;
-    toArray(BinarizeImg);//生成二值化的数组文件
-    //toArray(GrayscaleImg);//生成灰度图的数组文件
+    flip(GrayscaleImg, flip_horizontal_img, 1);//不知道为什么得到的图片在显示器上是水平的，所以在这里进行一次水平翻转
+    toArray(flip_horizontal_img);//生成二值化的数组文件
     cout << "生成完成！";
+    cout << "请在运行的根目录查看array.txt文件" << endl;
     waitKey(0);
     return 0;
 }
